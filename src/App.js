d2dcbdf921576f80548316cdebad3bd328dc1d4d
isidoreduc <isidoreducasse79@gmail.com>
2019-02-21 18:51:00 +0100

    basic components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div >
         <Navbar dark color='dark' >
           <div className='container'>
             <NavbarBrand href='/'> Ristorante KungFucius</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
